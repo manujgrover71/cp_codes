5f032cc9282fc850d1a4fbc73e4646cd0f109492
Manuj Grover <manujgrover71@gmail.com>
2020-05-07 15:41:00 +0530

    Add solution for problem `A - Puzzle Pieces` Link: https://codeforces.com/contest/1345/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1345/submission/79257086

---
+++ codeforces/1345/A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+#define ll long long
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    if(n == 1 || m == 1 || n == 2 && m == 2){
+        cout << "YES\n";
+    }else{
+        cout << "NO\n";
+    }
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
