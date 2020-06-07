efe00dc55e7e1ce7d374b721e9f68d40aac6c4e3
Manuj Grover <manujgrover71@gmail.com>
2020-06-07 16:05:00 +0530

    Add solution for problem `C - Table Decorations` Link: https://codeforces.com/contest/478/problem/C Tags: greedy, *1800 Ref: https://codeforces.com/contest/478/submission/82770842

---
+++ codeforces/478/C.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+void solve(){
+    ll arr[3], sum = 0;
+    for(int i = 0; i < 3; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr, arr + 3);
+
+    if(arr[2] >= 2 * (arr[0] + arr[1])){
+        cout << (arr[0] + arr[1]);
+    }else cout << sum / 3;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+}
