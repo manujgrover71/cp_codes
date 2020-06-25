5ff23831eb1f27b2a723561e3972ec66c9b54052
Manuj Grover <manujgrover71@gmail.com>
2020-06-25 11:37:00 +0530

    Add solution for problem `B - Queue at the School` Link: https://codeforces.com/contest/266/problem/B Tags: constructive algorithms, graph matchings, implementation, shortest paths, *800 Ref: https://codeforces.com/contest/266/submission/84942371

---
+++ codeforces/266/B.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+void solve() {
+    int n, t;
+    cin >> n >> t;
+    string str; cin >> str;
+
+    while(t--) {
+        for(int i = 0; i < str.length() - 1; i++) {
+            if(str[i] == 'B' && str[i+1] == 'G') swap(str[i], str[i+1]), i++;
+        }
+    }
+
+    cout << str;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+}
