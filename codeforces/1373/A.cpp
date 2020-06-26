4cea091d195c874adba6180d6c702cc65faa6571
Manuj Grover <manujgrover71@gmail.com>
2020-06-26 09:22:00 +0530

    Add solution for problem `A - Donut Shops` Link: https://codeforces.com/contest/1373/problem/A Tags: greedy, implementation, math, *1000 Ref: https://codeforces.com/contest/1373/submission/85087177

---
+++ codeforces/1373/A.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+void solve() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    if(a < c) {
+        cout << "1 ";
+    }else cout << "-1 ";
+
+    if(a * b > c) cout << b << '\n';
+    else cout << "-1\n";
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+
+}
