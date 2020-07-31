d6482a311e0dd4c23c8424fd6588fe326fa1f739
Manuj Grover <manujgrover71@gmail.com>
2020-07-31 18:59:00 +0530

    Add solution for problem `A - Remainder` Link: https://codeforces.com/contest/1165/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/1165/submission/88595757

---
+++ codeforces/1165/A.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+void solve() {
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    string str; cin >> str;
+
+    reverse(all(str));
+    int ans = 0;
+    for(int i = 0; i < x; i++) {
+        if(i == y) {
+            if(str[i] != '1')
+                ans++;
+        } 
+        else if(str[i] != '0') ans++;
+    }
+
+    cout << ans << '\n';
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+}
