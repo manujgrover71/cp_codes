7762ff9a6a91fe5e6876fd30e5f911b936b5e18a
Manuj Grover <manujgrover71@gmail.com>
2020-08-15 08:50:00 +0530

    Add solution for problem `A - Bad Triangle` Link: https://codeforces.com/contest/1398/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1398/submission/89982756

---
+++ codeforces/1398/A.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+void solve() {
+
+    int n; cin >> n;
+
+    ll a, b, c;
+
+    for(int i = 0; i < n; i++) {
+        if(!i) cin >> a;
+        else if(i == 1) cin >> b;
+        else cin >> c;
+    }
+
+    if(c < a + b) {
+        cout << "-1\n";
+    }else cout << "1 2 " << n << '\n';
+
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+
+}
