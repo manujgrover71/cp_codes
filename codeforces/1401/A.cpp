d5965994c89d121224e8c9fb16418e74257d431c
Manuj Grover <manujgrover71@gmail.com>
2020-08-22 09:52:00 +0530

    Add solution for problem `A - Distance and Axis` Link: https://codeforces.com/contest/1401/problem/A Tags: constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1401/submission/90652114

---
+++ codeforces/1401/A.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+// Check for number of Cases!!
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    if(n >= k) {
+        cout << (n - k) % 2 << '\n';
+    }else cout << (k - n) << '\n';
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
