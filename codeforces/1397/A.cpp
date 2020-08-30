fdf6c8cbe51089de9569a152d234bb63976aaf68
Manuj Grover <manujgrover71@gmail.com>
2020-08-30 20:09:00 +0530

    Add solution for problem `A - Juggling Letters` Link: https://codeforces.com/contest/1397/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1397/submission/91354380

---
+++ codeforces/1397/A.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+ll power(int x, unsigned int y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = res * x;
+        y >>= 1;
+        x *= x;
+    }
+    return res;
+}
+
+// Check for number of Cases!!
+void solve() {
+    int n; cin >> n;
+    // string str[n];
+
+    map<char, int> mp;
+
+    for(int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+        for(char j : str)
+            mp[j]++;
+    }
+
+    for(auto i : mp) {
+        if(i.second % n != 0) {
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
