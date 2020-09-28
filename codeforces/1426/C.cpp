b7ddbf28829557299856f807a4ce028026e61d4f
Manuj Grover <manujgrover71@gmail.com>
2020-09-28 13:57:00 +0530

    Add solution for problem `C - Increase and Copy` Link: https://codeforces.com/contest/1426/problem/C Tags: binary search, constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1426/submission/94075725

---
+++ codeforces/1426/C.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{ for (auto &v : vec) is >> v; return is; }
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+// Check for number of Cases!!
+void solve() {
+    ll num;
+
+    cin >> num;
+
+    if(num==1){
+        cout << "0\n";
+    }else {
+        ll ref = 1;
+        ll ans = 0;
+        ll x = 1;
+        while(ref < num){
+            if(ref + x >=num){
+                ans++;
+                break;
+            }
+            ref+=2 * x + 1;
+            ans+=2;
+            x++;
+        }
+        cout << ans <<"\n";
+    }
+    
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
