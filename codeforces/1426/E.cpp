9e1b107934c81a7f16e75c9b5d2c6b2b61540618
Manuj Grover <manujgrover71@gmail.com>
2020-09-28 15:44:00 +0530

    Add solution for problem `E - Rock, Paper, Scissors` Link: https://codeforces.com/contest/1426/problem/E Tags: brute force, constructive algorithms, flows, greedy, math, *1800 Ref: https://codeforces.com/contest/1426/submission/94112649

---
+++ codeforces/1426/E.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{ for (auto &v : vec) is >> v; return is; }
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve() {
+    ll n; cin >> n;
+    ll arr[3], brr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+    cin >> brr[0] >> brr[1] >> brr[2];
+
+    ll mx = 0, mn = 0;
+
+    for(int i = 0; i < 3; i++) {
+        mx += min(arr[i], brr[(i+1)%3]);
+    }
+
+    for(int i = 0; i < 3; i++) {
+        mn += max(0ll, arr[i] + brr[(i+1)%3] - n);
+    }
+
+    cout << mn << ' ' << mx << '\n';
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+      solve();
+    }
+}
