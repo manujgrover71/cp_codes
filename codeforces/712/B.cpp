369c73c5a329a2f72f684dadf8d4f6eefd1cea56
Manuj Grover <manujgrover71@gmail.com>
2020-10-29 14:27:00 +0530

    Add solution for problem `B - Memory and Trident` Link: https://codeforces.com/contest/712/problem/B Tags: implementation, strings, *1100 Ref: https://codeforces.com/contest/712/submission/97065797

---
+++ codeforces/712/B.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// ------------------------------------------------
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+template<typename T> 
+void dout(string name, T arg) {
+    cerr << arg << endl;
+}
+
+template<typename T1, typename... T2> 
+void dout(string names, T1 arg, T2... args) {
+    cerr << arg << " | ";
+    dout(names.substr(names.find(',') + 2), args...);
+}
+
+#ifndef ONLINE_JUDGE
+    #define debug(...) dout(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define debug(...) 
+#endif
+
+// --------------------------------------------------
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve() {
+    string str; cin >> str;
+
+    int l = 0, r = 0, u = 0, d = 0;
+
+    for(char ch : str) {
+        if(ch == 'L') {
+            if(r) r--;
+            else l++;
+        }else if(ch == 'R') {
+            if(l) l--;
+            else r++;
+        }else if(ch == 'U') {
+            if(d) d--;
+            else u++;
+        }else {
+            if(u) u--;
+            else d++;
+        }
+    }
+
+    if((l + r + d + u) & 1) {
+        cout << "-1";
+    }else {
+        cout << (l + r + d + u) / 2;
+    }
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    freopen("/ATOMCODES/err_output.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+      solve();
+    }
+}
