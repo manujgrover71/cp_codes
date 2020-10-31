27232e604a41774f6f2c334c7cd47422755a4844
Manuj Grover <manujgrover71@gmail.com>
2020-10-31 19:38:00 +0530

    Add solution for problem `C - K-Complete Word` Link: https://codeforces.com/contest/1332/problem/C Tags: dfs and similar, dsu, greedy, implementation, strings, *1500 Ref: https://codeforces.com/contest/1332/submission/97250532

---
+++ codeforces/1332/C.cpp
@@ -0,0 +1,127 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// ------------------------------------------------
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+template<typename T> 
+void dout(string name, T arg) {
+    cerr << arg << endl;
+}
+
+template<typename T1, typename... T2> 
+void dout(string names, T1 arg, T2... args) {
+    cerr << arg << " | ";
+    dout(names.substr(names.find(',') + 2), args...);
+}
+
+#ifndef ONLINE_JUDGE
+    #define debug(...) dout(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define debug(...) 
+#endif
+
+// --------------------------------------------------
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+// Check for number of Cases!!
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+
+    string str; cin >> str;
+
+    vll vis(n, false);
+
+    for(int i = 0; i < n; i++) {
+        if(!vis[i]) {
+            for(int j = i; j < n; j += k) {
+                vis[j] = min(i+1, j+1);
+            }
+        }
+    }
+
+    for(int i = 0, j = n-1; i < j; i++, j--) {
+        vis[i] = vis[j] = min(vis[i], vis[j]);
+    }
+
+    vector<vector<ll>> freq(n + 1, vector<ll> (26, 0));
+    vector<pair<ll, ll>> ref(n, {0, 0});
+
+    for(int i = 0; i < n; i++) {
+        ll x = vis[i];
+        freq[x][str[i] - 'a']++;
+        ll currMax = max(ref[x].first, freq[x][str[i]-'a']);
+        ll newSize = ref[x].second + 1;
+
+        ref[x] = {currMax, newSize};
+    }
+
+    ll ans = 0;
+
+    for(auto i : ref) {
+        debug(i.first, i.second);
+        ans += (i.second - i.first);
+    }
+
+    cout << ans << '\n';
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    freopen("/ATOMCODES/err_output.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
