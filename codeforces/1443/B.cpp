bb6009a86f666d3a42af60cd2c4580bf44c255e3
Manuj Grover <manujgrover71@gmail.com>
2020-11-02 20:34:00 +0530

    Add solution for problem `B - Saving the City` Link: https://codeforces.com/contest/1443/problem/B Tags: dp, greedy, math, sortings, *1300 Ref: https://codeforces.com/contest/1443/submission/97452195

---
+++ codeforces/1443/B.cpp
@@ -0,0 +1,138 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// ------------------------------------------------
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+template<typename T> 
+void dout(string name, T arg) {
+    cerr << arg << endl;
+}
+
+template<typename T1, typename... T2> 
+void dout(string names, T1 arg, T2... args) {
+    cerr << arg << " | ";
+    dout(names.substr(names.find(',') + 2), args...);
+}
+
+#ifndef ONLINE_JUDGE
+    #define debug(...) dout(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define debug(...) 
+#endif
+
+// --------------------------------------------------
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve(){
+	ll a,b;
+    cin >> a >> b;
+    string str; cin >> str;
+
+	ll ans=0;
+	
+    ll n = str.size();
+	int i=0;
+
+    for( i = 0; i < n; ) {
+        if(str[i] == '0') i++;
+        else break;
+    }
+
+    for( ; i < n; ) {
+        if(str[i] == '1') {
+            i++;
+            ans = a;
+        }else break;
+    }
+
+	while(i < n){
+		ll ref = 0;
+        for( ; i < n ; ) {
+            if(str[i] == '0') {
+                i++;
+                ref += b;
+            }else break;
+        }
+
+        if(i == n) {
+            cout << ans << '\n';
+            return;
+        }
+
+
+		while(i < n && str[i]=='1'){
+			i++;
+		}
+
+		if(ref <= a){
+			ans += ref;
+		}
+		else{
+			ans += a;
+		}
+	}
+
+	cout << ans <<"\n";
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    freopen("/ATOMCODES/err_output.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
