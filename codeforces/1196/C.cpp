acc66b504f0095d9a2286f1517fa815a7870a9d9
Manuj Grover <manujgrover71@gmail.com>
2020-11-03 13:49:00 +0530

    Add solution for problem `C - Robot Breakout` Link: https://codeforces.com/contest/1196/problem/C Tags: implementation, *1500 Ref: https://codeforces.com/contest/1196/submission/97539317

---
+++ codeforces/1196/C.cpp
@@ -0,0 +1,118 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// ------------------------------------------------
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+template<typename T> 
+void dout(string name, T arg) {
+    cerr << arg << endl;
+}
+
+template<typename T1, typename... T2> 
+void dout(string names, T1 arg, T2... args) {
+    cerr << arg << " | ";
+    dout(names.substr(names.find(',') + 2), args...);
+}
+
+#ifndef ONLINE_JUDGE
+    #define debug(...) dout(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define debug(...) 
+#endif
+
+// --------------------------------------------------
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = 1e5;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve() {
+    ll n; cin >> n;
+
+    ll maxLeft = -INF, maxRight = INF, maxUp = INF, maxDown = -INF;
+
+    for(int i = 0; i < n; i++) {
+        ll x, y, f1, f2, f3, f4;
+        // l u r d
+        cin >> x >> y >> f1 >> f2 >> f3 >> f4;
+
+        if(!f1) {
+            maxLeft = max(x, maxLeft);
+        }
+
+        if(!f3) {
+            maxRight = min(x, maxRight);
+        }
+
+        if(!f2) {
+            maxUp = min(maxUp, y);
+        }
+
+        if(!f4) {
+            maxDown = max(maxDown, y);
+        }
+    }
+
+    if(maxLeft <= maxRight && maxDown <= maxUp)
+        cout << "1 " <<  maxLeft << ' ' << maxDown << '\n';
+    else cout << "0\n";
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    freopen("/ATOMCODES/err_output.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
