6e9444e340d10d8c8d9ea0bd3b6aeb120df05fa9
Manuj Grover <manujgrover71@gmail.com>
2020-11-05 15:26:00 +0530

    Add solution for problem `B - Urbanization` Link: https://codeforces.com/contest/735/problem/B Tags: greedy, number theory, sortings, *1100 Ref: https://codeforces.com/contest/735/submission/97691598

---
+++ codeforces/735/B.cpp
@@ -0,0 +1,116 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// ------------------------------------------------
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+template<typename T> 
+void dout(string name, T arg) {
+    cerr << arg << endl;
+}
+
+template<typename T1, typename... T2> 
+void dout(string names, T1 arg, T2... args) {
+    cerr << arg << " | ";
+    dout(names.substr(names.find(',') + 2), args...);
+}
+
+#ifndef ONLINE_JUDGE
+    #define debug(...) dout(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define debug(...) 
+#endif
+
+// --------------------------------------------------
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve() {
+    ll n, n1, n2;
+    cin >> n >> n1 >> n2;
+
+    vll arr(n);
+
+    cin >> arr;
+
+    sort(rall(arr));
+
+    if(n1 > n2) swap(n1, n2);
+
+    double a = 0, b = 0;
+
+    for(int i = 0; i < n1; i++) {
+        a += arr[i];
+    }
+
+    for(int i = n1; i < n1 + n2; i++) {
+        b += arr[i];
+    }
+
+    a = (1.0 * a / n1);
+    b = (1.0 * b / n2);
+
+    debug(a, b);
+
+    cout << fixed << setprecision(10) << (a + b);
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    freopen("/ATOMCODES/err_output.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+      solve();
+    }
+}
