d1d7d9b1a6fd7ff6ac74408fe5de82b7931e349a
Manuj Grover <manujgrover71@gmail.com>
2020-11-17 20:33:00 +0530

    Add solution for problem `B - Sum of Medians` Link: https://codeforces.com/contest/1440/problem/B Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1440/submission/98701609

---
+++ codeforces/1440/B.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// ------------------------------------------------
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+template<typename T> 
+void dout(string name, T arg) {
+    cerr << arg << endl;
+}
+
+template<typename T1, typename... T2> 
+void dout(string names, T1 arg, T2... args) {
+    cerr << arg << " | ";
+    dout(names.substr(names.find(',') + 2), args...);
+}
+
+#ifndef ONLINE_JUDGE
+    #define debug(...) dout(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define debug(...) 
+#endif
+
+// --------------------------------------------------
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+    ll res = 1;
+    while(y > 0){
+        if(y & 1) res = (res * x) % nod;
+        y >>= 1;
+        x = (x * x) % nod;
+    }
+    return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve() {
+    ll n, k; cin >> n >> k;
+    
+    vll arr(n*k);
+    cin >> arr;
+    
+    ll ans=0;
+	ll ref=ceil(n/2.0);
+    
+	if(n & 1)	ref--;
+
+	int r1 = ref;
+	int r2 =k;
+	for(int i = n*k-1; i >= 0 && r2; i--){
+		if(r1 == 0){
+			r2--;
+			r1 = ref;
+			ans += arr[i];
+			continue;
+		}
+		r1--;
+	} 
+    
+    
+	cout << ans << "\n";
+    
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/ATOMCODES/input.txt", "r", stdin);
+    freopen("/ATOMCODES/output.txt", "w", stdout);
+    freopen("/ATOMCODES/err_output.txt", "w", stderr);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+}
