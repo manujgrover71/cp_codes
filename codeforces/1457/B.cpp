77f25c47691b20b8ce31c2999ad77f2e4151b0a8
Manuj Grover <manujgrover71@gmail.com>
2020-11-29 12:52:00 +0530

    Add solution for problem `B - Repainting Street` Link: https://codeforces.com/contest/1457/problem/B Tags: brute force, greedy, *1100 Ref: https://codeforces.com/contest/1457/submission/99849929

---
+++ codeforces/1457/B.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+	ll res = 1;
+	while(y > 0){
+		if(y & 1) res = (res * x) % nod;
+		y >>= 1;
+		x = (x * x) % nod;
+	}
+	return (res % nod);
+}
+
+
+
+// Check for number of Cases!!
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	
+	vll arr(n);
+	
+	for(int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	ll ans = INF;
+	ll curr = 0;
+	
+	for(int j = 1; j <= 100; j++) {
+		curr = 0;
+		for(int i = 0; i < n; ) {
+			if(arr[i] != j) {
+				i += k;
+				curr++;
+			}else i++;
+		}
+		ans = min(ans, curr);
+	}
+	
+	cout << ans << '\n';
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("/ATOMCODES/input.txt", "r", stdin);
+	freopen("/ATOMCODES/output.txt", "w", stdout);
+	freopen("/ATOMCODES/err_output.txt", "w", stderr);
+	#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t = 1;
+	cin >> t;
+	while(t--) {
+	  solve();
+	}
+}
