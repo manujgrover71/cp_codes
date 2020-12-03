af088502cf014a456d9a0ea2e75679a8e94a7ee9
Manuj Grover <manujgrover71@gmail.com>
2020-12-03 15:13:00 +0530

    Add solution for problem `A - Stock Arbitraging` Link: https://codeforces.com/contest/1150/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1150/submission/100257563

---
+++ codeforces/1150/A.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+
+using namespace std;
+
+template <typename T> istream &operator>>
+(istream &is, vector<T> &vec) 
+{   for (auto &v : vec) is >> v; return is; }
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ll long long
+#define mod 1000000007
+#define nod 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+
+
+// D R U L
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int mx = 1e6 + 5;
+const int INF = mod;
+
+ll power(ll x, ll y){
+	ll res = 1;
+	while(y > 0){
+		if(y & 1) res = (res * x) % nod;
+		y >>= 1;
+		x = (x * x) % nod;
+	}
+	return (res % nod);
+}
+
+
+// Check for number of Cases!!
+void solve() {
+	ll n, m, k;
+	cin >> n >> m >> k;
+	
+	vll arr(n), brr(m);
+	cin >> arr >> brr;
+	
+	sort(all(arr));
+	sort(rall(brr));
+	
+	ll ans = k;
+	
+	if(arr[0] < brr[0]) {
+		int count = k / arr[0];
+		// cout << count << '\n';
+		ans -= count * arr[0];
+		ans += count * brr[0];
+	}
+	
+	cout << ans << '\n';
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("/ATOMCODES/input.txt", "r", stdin);
+	freopen("/ATOMCODES/output.txt", "w", stdout);
+	freopen("/ATOMCODES/err_output.txt", "w", stderr);
+	#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t = 1;
+	// cin >> t;
+	while(t--) {
+	  solve();
+	}
+}
